86a37aeae4aa391e5bd30bd30c61e5f0d4b1681e
Foysal Mahmood <foysalcse033@gmail.com>
2024-07-15 18:29:54 +0600

    create new api for user post and update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { ObjectId } = require('mongodb');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
@@ -29,10 +30,43 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const database = client.db('research-and-development');
-    const userCollection = database.collection("user");
+    const userCollection = database.collection("users");
 
 
+    app.post('/users', async (req, res) => {
+      const userData = req.body;
+    
+      try {
+        if (userData._id) {
+          // Update operation
+          const userId = userData._id;
+          delete userData._id; // Remove _id from the userData to prevent overriding it
+    
+          const result = await userCollection.updateOne(
+            { _id: new ObjectId(userId) },
+            { $set: userData }
+          );
+    
+          if (result.matchedCount === 0) {
+            return res.status(404).send({ error: 'User not found' });
+          }
+    
+          res.send(result);
+        } else {
+          // Create operation
+          const result = await userCollection.insertOne(userData);
+          res.status(201).send(result);
+        }
+      } catch (error) {
+        res.status(500).send({ error: 'Failed to create or update user' });
+      }
+    });
 
+    app.get('/users', async(req, res) => {
+      const getUser = userCollection.find();
+      const result = await getUser.toArray();
+      res.send(result);
+    });
 
 
   } finally {
